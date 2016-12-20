e025dc9e1ac7f50864b1b1f20f66be1f1a218793
sgolikov <sgolikov.im@gmail.com>
2016-12-20 15:31:45 +0300

    Add <ts-header> component

--- src/app/layout/header/header.component.js
+++ src/app/layout/header/header.component.js
@@ -4,5 +4,16 @@ angular
     .module('ts.layout.header')
     .component('tsHeader', {
         templateUrl: 'app/layout/header/header.component.html',
-        controller: function() {}
+        bindings: {
+            "mainNav": "<",
+            "subNav": "<",
+            "onClick": "&"
+        },
+        controller: function() {
+            var vm = this;
+
+            vm.onItemClick = function(item) {
+                vm.onClick({item: item});
+            };
+        }
     });
--- src/app/layout/header/header.module.js
+++ src/app/layout/header/header.module.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('ts.layout.header', []);
+angular.module('ts.layout.header', [
+    'ts.components.navbar'
+]);
